a2c9551fe12275246f542f32ca3afb1b54e1a752
ShahadAlharbi21 <shahad_100hr@outlook.com>
2025-07-19 00:52:51 +0100

    Did project card design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,17 +10,80 @@ function Projects(){
     const githubIcon = theme === 'light' ? githubLight : githubDark;
     return(<>
     <NavBar/>
+
     <section className={styles.projectsSection}>
-        <h1>Projects</h1>
-            <section className={styles.ProjectCard}>
-                <h2>Loan Classification and Soybean Yield Prediction</h2>
-                <h3> Coursework Project | Mar 2025 – Apr 2025</h3>
-                <section lassName={styles.stack}>
+             <h1>Projects</h1>
+            <div className={styles.ProjectCard}>
+                <div className={styles.projectInner}>
+                <div className={styles.leftContent}>
+                    <h2>Loan Classification and Soybean Yield Prediction</h2>
+                    <p>Coursework Project | Mar 2025 – Apr 2025</p>
+                    <p>
+                    A machine learning project for classifying loan approval and predicting soybean yield based on environmental data.
+                    </p>
+                </div>
+
+                <div className={styles.rightTechCard}>
+                    <section>
                     <h3>Languages</h3>
-                    <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=FFD43B" alt="Python" className={styles.badgeImage} />
-                </section>
-            </section>
-    </section>
+                    <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=FFD43B" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Frameworks</h3>
+                    <img src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=000" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Databases</h3>
+                    <img src="https://img.shields.io/badge/SQLite-003B57?style=for-the-badge&logo=sqlite&logoColor=white" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Tools</h3>
+                    <img src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" />
+                    </section>
+                </div>
+                </div>
+            </div>
+
+
+            <div className={styles.ProjectCard}>
+                <div className={styles.projectInner}>
+                <div className={styles.leftContent}>
+                    <h2>Loan Classification and Soybean Yield Prediction</h2>
+                    <p>Coursework Project | Mar 2025 – Apr 2025</p>
+                    <p>
+                    A machine learning project for classifying loan approval and predicting soybean yield based on environmental data.
+                    </p>
+                </div>
+
+                <div className={styles.rightTechCard}>
+                    <section>
+                    <h3>Languages</h3>
+                    <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=FFD43B" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Frameworks</h3>
+                    <img src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=000" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Databases</h3>
+                    <img src="https://img.shields.io/badge/SQLite-003B57?style=for-the-badge&logo=sqlite&logoColor=white" />
+                    </section>
+                    <hr />
+                    <section>
+                    <h3>Tools</h3>
+                    <img src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" />
+                    </section>
+                </div>
+                </div>
+            </div>
+   </section>
+
+
     <Footer/>
     </>
     );
