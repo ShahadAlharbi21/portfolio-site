70a3f0418a64c21abbe1c0d2b701d51508fc35ff
ShahadAlharbi21 <shahad_100hr@outlook.com>
2025-07-19 13:29:50 +0100

    fixed skills section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,28 +8,30 @@ import Footer from './Footer';
 function Contact(){
     return(<>
     <NavBar/>
-    <div className={styles.ContactCard}>
-        <h1>Contact Details</h1>
-    </div>
-        <section className={styles.ContactCardSection}>
-        <span>
-            <a href='https://github.com/ShahadAlharbi21' target="_blank" rel="noopener noreferrer">
-            <img src={githubDark} alt='Github icon' />
-            </a>
-            <div className={styles.ImgH}>
-            <h3>Github</h3>
-            </div>
-        </span>
+    <div className={styles.ContactCardWrapper}>
+        <div className={styles.ContactCard}>
+            <h1>Contact Details</h1>
+        </div>
+            <section className={styles.ContactCardSection}>
+            <span>
+                <a href='https://github.com/ShahadAlharbi21' target="_blank" rel="noopener noreferrer">
+                <img src={githubDark} alt='Github icon' />
+                </a>
+                <div className={styles.ImgH}>
+                <h3>Github</h3>
+                </div>
+            </span>
 
-        <span>
-            <a href="mailto:shahad_100hr@outlook.com " target="_blank " rel="noopener noreferrer">
-            <img src={email} alt="Email Logo" style={{ width: '64px', height: '64px' }} />
-            </a>
-            <div className={styles.ImgH}>
-            <h3>Email</h3>
-            </div>
-        </span>
-        </section>
+            <span>
+                <a href="mailto:shahad_100hr@outlook.com " target="_blank " rel="noopener noreferrer">
+                <img src={email} alt="Email Logo" style={{ width: '64px', height: '64px' }} />
+                </a>
+                <div className={styles.ImgH}>
+                <h3>Email</h3>
+                </div>
+            </span>
+            </section>
+        </div>
         <Footer/>
     </>
     );
