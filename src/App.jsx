02dea570253185de76851c43c2c7d75877a9bbef
ShahadAlharbi21 <shahad_100hr@outlook.com>
2025-09-01 20:52:33 +0100

    fixed main link2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import About from './components/About'
 import Projects from './components/Projects'
 import Certificates from './components/Certificates'
@@ -8,6 +8,7 @@ import Contact from './components/Contact'
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/about" replace />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/certificates" element={<Certificates />} />
