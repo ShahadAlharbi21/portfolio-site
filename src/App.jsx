80d95e18311223f3a3d2ee779b29b7c95ea6bf93
ShahadAlharbi21 <shahad_100hr@outlook.com>
2025-09-01 20:40:59 +0100

    fixed urls2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About'
-import Home from './components/Home'
 import Projects from './components/Projects'
 import Certificates from './components/Certificates'
 import Contact from './components/Contact'
@@ -9,7 +8,7 @@ import Contact from './components/Contact'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<About />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/certificates" element={<Certificates />} />
